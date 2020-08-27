93d65969c3fc824a94a09293027e875bc3d9ed5e
minghua <minghua@gmail.com>
2020-08-27 10:53:46 +0800

    0827

--- 0825/ch14/php_mysql4.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	header("Content-Type: text/html; charset=utf-8");
-	include("connMysql.inc");
-	$seldb = @mysql_select_db("class");
-	if (!$seldb) die("資料庫選擇失敗！");
-	$sql_query = "SELECT * FROM `students`";
-	$result = mysql_query($sql_query);
-?>
--- 0825/ch14/php_mysql5.php
+++
@@ -1,7 +0,0 @@
-<?php
-	header("Content-Type: text/html; charset=utf-8");
-	include("connMysql.inc");
-	$seldb = "class";
-	$sql_query = "SELECT * FROM `students`";
-	$result = mysql_db_query($seldb,$sql_query);
-?>
